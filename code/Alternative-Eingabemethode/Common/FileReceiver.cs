5efbce53dc2c96573fdbbaa36620882dfde9d340
Jonas Schwammberger <hooovercat@gmail.com>
2014-03-27 16:13:16 +0100

    implemented filetransfer

--- code/Alternative-Eingabemethode/Common/FileReceiver.cs
+++ code/Alternative-Eingabemethode/Common/FileReceiver.cs
@@ -18,16 +18,66 @@ namespace Common
     /// </summary>
     public class FileReceiver
     {
-        public static void Receive(DirectoryInfo saveDir, Socket sender, byte[] buffer) {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="saveDir"></param>
+        /// <param name="receiver"></param>
+        /// <param name="buffer">has to be able to contain the Filename String in UTF-8</param>
+        public static void Receive(DirectoryInfo saveDir, Socket receiver, byte[] buffer) {
             if (!saveDir.Exists)
                 saveDir.Create();
 
-            int read = sender.Receive(buffer,8,SocketFlags.None);
+            string name;
+
+            //readTotal header
+            ReadExact(receiver, 8, buffer, 0);
             int stringLen = BitConverter.ToInt32(buffer, 0);
             int dataLen = BitConverter.ToInt32(buffer, 4);
 
-            //read string
-            StringBuilder name = new StringBuilder(); 
+            //readTotal string
+            ReadExact(receiver, stringLen, buffer, 0);
+            name = Encoding.UTF8.GetString(buffer, 0, stringLen);
+
+            //readTotal binary and save to file
+            BinaryWriter w = new BinaryWriter(File.OpenWrite(Path.Combine(saveDir.FullName, name)));
+            try
+            {
+                int readTotal = 0;
+
+                while (readTotal < dataLen)
+                {
+                    //ensures that no more bytes are read than needed.
+                    int byesToRead = dataLen - readTotal > buffer.Length ? buffer.Length : dataLen - readTotal;
+                    int read = receiver.Receive(buffer, byesToRead, SocketFlags.None);
+
+                    w.Write(buffer, 0, read);
+                    readTotal += read;
+                }
+
+            }
+            finally
+            {
+                w.Close();
+            }
+           
+        }
+
+        /// <summary>
+        /// Reads an exact number of bytes and writes them in the buffer
+        /// </summary>
+        /// <param name="receiver"></param>
+        /// <param name="bytecount">minimum Number of Bytes to readTotal</param>
+        /// <param name="buffer">to write in</param>
+        /// <param name="offset">offset in buffer, starts writing at specified index</param>
+        public static void ReadExact(Socket receiver, int bytecount, byte[] buffer, int offset)
+        {
+            int read = 0;
+
+            while (read < bytecount)
+            {
+                read += receiver.Receive(buffer, offset+read, bytecount-read, SocketFlags.None);
+            }
         }
 
     }
