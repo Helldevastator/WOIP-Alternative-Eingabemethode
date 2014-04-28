47568dd90811bb5ae83d79390b73e711e4ceaab2
Jonas Schwammberger <hooovercat@gmail.com>
2014-04-28 10:36:29 +0200

    Worked on Client

--- code/Alternative-Eingabemethode/Client/DisplayWindow.cs
+++ code/Alternative-Eingabemethode/Client/DisplayWindow.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using Common;
+using Client.ResourceHandler;
 
 namespace Client
 {
+    /// <summary>
+    /// Represents a Window on the Display.
+    /// </summary>
     class DisplayWindow
     {
+        private ClientState state;
+        private IResourceHandler resource;
+
+        public DisplayWindow()
+        {
+        }
+
+        public void Update(ClientState state) {
+            this.state = state;
+        }
+
+        /// <summary>
+        /// Paint Window on Display
+        /// </summary>
+        /// <param name="g"></param>
+        public void OnPaint(Graphics g)
+        {
+            resource.OnPaint(g, state);
+        }
+
+        /// <summary>
+        /// Fired when Resource is Loaded
+        /// </summary>
+        /// <param name="resource"></param>
+        private void ResourceLoadedListener(IResourceHandler resource)
+        {
+            lock(this.resource)
+                this.resource = resource;
+        }
+
     }
 }
---
+++ code/Alternative-Eingabemethode/Client/ResourceHandler/IResourceHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Common;
+
+namespace Client.ResourceHandler
+{
+    interface IResourceHandler
+    {
+        public void OnPaint(Graphics g, WindowState state);
+        
+    }
+}
