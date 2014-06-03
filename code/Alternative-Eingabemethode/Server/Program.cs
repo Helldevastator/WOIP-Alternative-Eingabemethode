a00479cb9dd409d6b9f87ff46acb00c5d6bc5b9b
Jonas Schwammberger <hooovercat@gmail.com>
2014-06-03 14:14:01 +0200

    added stuff for demo

--- code/Alternative-Eingabemethode/Server/Program.cs
+++ code/Alternative-Eingabemethode/Server/Program.cs
@@ -19,15 +19,23 @@ namespace Server
         {
             System.Console.WriteLine("Starting server");
 
+            string[] configuration = System.IO.File.ReadAllLines("client_conf.txt");
+            Client[] clients = new Client[4];
+
             //once client
             System.Console.WriteLine("Establishing connection to clients");
-            EndPoint resourceEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6556);
-            EndPoint updateEnd = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 5665);
+            EndPoint resourceEnd = new IPEndPoint(IPAddress.Parse(configuration[1]), 6556);
+            EndPoint updateEnd = new IPEndPoint(IPAddress.Parse(configuration[1]), 5665);
             Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             s.Connect(updateEnd);
-            Client c = new Client(0,s, resourceEnd, 1920, 1080, 30, 20);
-            Client[] clients = new Client[4];
-            clients[0] = c;
+            clients[0] = new Client(0, s, resourceEnd, configuration[2], configuration[3], configuration[4], configuration[5]);
+
+            resourceEnd = new IPEndPoint(IPAddress.Parse(configuration[7]), 6556);
+            updateEnd = new IPEndPoint(IPAddress.Parse(configuration[7]), 5665);
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            s.Connect(updateEnd);
+            clients[3] = new Client(3, s, resourceEnd, configuration[8], configuration[9], configuration[10], configuration[11]);
+
 
             System.Console.WriteLine("Establishing connection to wiimote");
             List<WiimoteAdapter> controllers = new List<WiimoteAdapter>();
@@ -44,7 +52,6 @@ namespace Server
             Random rand = new Random();
             foreach (FileInfo f in dir.GetFiles())
             {
-                
                 int resId = resServer.AddResource(f, 0);
                 Image im = new Bitmap(f.FullName);
                 int x = rand.Next(1920);
